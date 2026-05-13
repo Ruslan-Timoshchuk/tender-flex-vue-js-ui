ccda47104c556a4be2d9f62dddbce6a0faec5d04
ruslan-timoshchuk <ruslan.timoshchuk.arc@gmail.com>
2026-05-13 23:04:22 +0200

    some adjustments

--- src/services/country.api.js
+++ src/services/country.api.js
@@ -3,6 +3,6 @@ import axiosApiClient from "@/services/axios.api.client";
 const URL_COUNTRIES_ALL = 'api/v1/countries';
 
 export const findAllCountries = async () => {
-    const tendersCountResponse = await axiosApiClient.get(`${URL_COUNTRIES_ALL}`);
-    return tendersCountResponse.data;
+    const countriesResponse = await axiosApiClient.get(`${URL_COUNTRIES_ALL}`);
+    return countriesResponse.data;
 }
