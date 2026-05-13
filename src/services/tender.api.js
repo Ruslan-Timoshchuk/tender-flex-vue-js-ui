001e0084905e5b29ff3391933ac432ff086d456b
ruslan-timoshchuk <ruslan.timoshchuk.arc@gmail.com>
2026-05-13 13:33:07 +0200

    adjusted contractor tenders api

--- src/services/tender.api.js
+++ src/services/tender.api.js
@@ -2,6 +2,7 @@ import axiosApiClient from "@/services/axios.api.client";
 
 const URL_CONTRACTOR_COUNT = 'api/v1/tenders/contractor-count';
 const URL_COUNT_ALL = 'api/v1/tenders/count-all';
+const URL_CONTRACTOR_TENDERS_PAGE = 'api/v1/tenders/contractor-page';
 
 export const countTendersByContractor = async (contractorId) => {
     const tendersCountResponse = await axiosApiClient.get(`${URL_CONTRACTOR_COUNT}/${contractorId}`);
@@ -11,4 +12,10 @@ export const countTendersByContractor = async (contractorId) => {
 export const countAllTenders = async () => {
     const tendersCountResponse = await axiosApiClient.get(`${URL_COUNT_ALL}`);
     return tendersCountResponse.data;
+}
+
+export const findTendersByContractor = async (contractorId, page, pageSize) => {
+    const tendersPageResponse = await axiosApiClient
+        .get(`${URL_CONTRACTOR_TENDERS_PAGE}/${contractorId}?page=${page}&pageSize=${pageSize}`);
+    return tendersPageResponse.data;
 }
