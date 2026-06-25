3f433831d980b7ed8fc1813f02e5ea250814efca
ruslan-timoshchuk <ruslan.timoshchuk.arc@gmail.com>
2026-06-26 01:19:41 +0200

    reefactored contractor offers table

---
+++ src/components/offer/ContractorOfferSummary.vue
@@ -0,0 +1,76 @@
+<template>
+  <ToolBarTitle
+    title="Offers">
+  </ToolBarTitle>
+
+  <v-container fluid class="d-flex align-center justify-center mt-n12">
+  <v-card elevation="8" width="1000">
+    <div v-if="contractorOffersStore.isEmpty">
+      <EmptyTableTitle
+        message="“There are no received Offers”">
+      </EmptyTableTitle>
+    </div>
+    <div v-else>
+      <OffersSummaryTable
+        companyName="Oficial Name"
+        field="Field"
+        price="Price"
+        country="Country"
+        date="Received Date"
+        status="Status"
+        :offers="contractorOffersStore.offers"
+        @load-more-on-scroll="loadMoreByContractor"
+        @select-offer="(offer) => navigateToTender(offer.tender)"
+      ></OffersSummaryTable>
+    </div>
+  </v-card>
+  </v-container>
+</template>
+
+<script>
+import ToolBarTitle from "@/components/childs/ToolBarTitle.vue"
+import EmptyTableTitle from "@/components/childs/EmptyTableTitle.vue"
+import OfferSummaryTable from "@/components/offer/childs/OfferSummaryTable.vue"
+import { useContractorOffersStore } from "@/stores/offer.contractor.pegeable.store"
+import { exceptionAlert } from "@/components/alerts";
+
+export default {
+  components:{
+    ToolBarTitle,
+    EmptyTableTitle,
+    OfferSummaryTable,
+  },
+
+  data: () => ({
+    contractorOffersStore: useContractorOffersStore(),
+    pageSize: 10,
+    loading: false,
+    exceptionAlert
+  }),
+
+  methods: {
+    async loadMoreByContractor(value) {
+      try {
+        const target = value.target;
+        const scrollPosition = target.scrollTop + target.clientHeight;
+        const isBottomReached = scrollPosition >= target.scrollHeight - 50;
+        const currentPage = this.contractorOffersStore.currentPage;
+        const totalPages = this.contractorOffersStore.totalPages;
+        if (isBottomReached && !this.loading && currentPage < totalPages) {
+          this.loading = true;
+          const nextPage = currentPage + 1;
+          await this.contractorOffersStore.loadMoreOffersByContractor(nextPage, this.pageSize);
+          this.loading = false;
+        }
+      } catch (error) {
+        this.loading = false;
+        this.exceptionAlert.activateAlert(error);
+      }
+    }
+  },
+
+ async mounted() {
+    await this.contractorOffersStore.loadOffersByContractor(this.contractorOffersStore.currentPage, this.pageSize);
+  }
+}
+</script>
